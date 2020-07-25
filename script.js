6a95f2abf43cdd57052e45c77a2bdaec46ab1fca
Annie Huang <annie.hn@gmail.com>
2020-07-25 17:59:51 +1000

    Preferred switch statement

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ for(var i = 0; i < themeDots.length; i++) {
 }
 
 setTheme = (mode) => {
-  if (mode === 'light') {
-    document.getElementById('theme-style').href = 'default.css'
-  }
-
-  if(mode === 'blue'){
-    document.getElementById('theme-style').href = 'blue.css'
-  }
-
-  if(mode === 'green'){
-    document.getElementById('theme-style').href = 'green.css'
-  }
-
-  if(mode === 'purple'){
-    document.getElementById('theme-style').href = 'purple.css'
+  switch (mode) {
+    case 'light':
+      document.getElementById('theme-style').href = 'default.css';
+      break;
+    case 'blue':
+      document.getElementById('theme-style').href = 'blue.css';
+      break;
+    case 'green':
+      document.getElementById('theme-style').href = 'green.css';
+      break;
+    case 'purple':
+      document.getElementById('theme-style').href = 'purple.css';
+      break;
+    default:
+      document.getElementById('theme-style').href = 'default.css';
   }
 }
