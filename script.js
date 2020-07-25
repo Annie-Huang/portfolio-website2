321333e6d6e2456dc576bbecb9cd140c099e5f62
Annie Huang <annie.hn@gmail.com>
2020-07-25 17:56:52 +1000

    Choose different color when the theme element is clicked

--- script.js
+++ script.js
@@ -8,5 +8,24 @@ for(var i = 0; i < themeDots.length; i++) {
   themeDots[i].addEventListener('click', function(){ // Cannot use ES6 syntax too.
     let mode = this.dataset.mode;
     console.log('Option clicked', mode);
+    setTheme(mode);
   })
+}
+
+setTheme = (mode) => {
+  if (mode === 'light') {
+    document.getElementById('theme-style').href = 'default.css'
+  }
+
+  if(mode === 'blue'){
+    document.getElementById('theme-style').href = 'blue.css'
+  }
+
+  if(mode === 'green'){
+    document.getElementById('theme-style').href = 'green.css'
+  }
+
+  if(mode === 'purple'){
+    document.getElementById('theme-style').href = 'purple.css'
+  }
 }
